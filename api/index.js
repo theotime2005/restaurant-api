24ccdce8ef81fc55575e0316c7255df8c3f9b5a5
theotime2005 <theotime.berthod@epitech.digital>
2023-10-25 17:24:09 +0200

    The database is create! now just todo the route!

--- api/index.js
+++ api/index.js
@@ -15,21 +15,44 @@ const connection = mysql.createConnection({
 
 connection.connect()
 
-app.post('/api/post-item', (req, res) => {
-    const mysqlRequest = `INSERT INTO Article VALUES (${req.body.name}, ${req.body.price}, ${req.body.categorie});`;
-    console.log(mysqlRequest);
+app.post('POST/item', (req, res) => {
+    const mysqlRequest = 'INSERT INTO Items (name, description, id_category) VALUES ("${req.body.name}", "${req.body.description}, ${req.body.price}", "${req.body.category}");';
     connection.query(mysqlRequest, (err, rows, fields) => {
         if (err) throw err;
-        res.send(rows);
+        res.send("Item added!");
     })
 })
 
-app.get('/api/get-items', async (req, res) => {
-    const mysqlRequest = "SELECT * FROM Article";
+app.get('/items/get-items', async (req, res) => {
+    const mysqlRequest = "SELECT * FROM Items";
     connection.query(mysqlRequest, (err, rows, fields) => {
         if (err) throw err;
         console.log(rows);
         res.send(rows);
+    })
+})
+
+app.delete('/items/delete-item', (req, res) => {
+    const mysqlRequest = `DELETE "${req.body.name}" FROM ITEMS;`;
+    connection.query(mysqlRequest, (err, rows, fields) => {
+        if (err) throw err;
+        res.send("Element deleted")
+    })
+})
+
+app.delete('/items/delete-items', (req, res) => {
+    const mysqlRequest = 'DELETE  FROM "Items";';
+    connection.query(mysqlRequest, (err, rows, fields) => {
+        if (err) throw err;
+        res.send("All items deleted");
+    })
+})
+
+app.post('/categorries/post-categorie', (req, res) => {
+    const mysqlRequest = 'INSERT INTO Categories ("name", "description") VALUES("${req.body.name}", "${req.body.description}");';
+    connection.query(mysqlRequest, (err, rows, fields) => {
+        if (err) throw err;
+        res.send("Category added");
     })
 })
 
---
+++ data/category.sql
@@ -0,0 +1,5 @@
+-- Ajouter les catégories principales à la table Categories
+
+INSERT INTO Categories(name) VALUES ("Entrée");
+INSERT INTO Categories(name) VALUES ("plat");
+INSERT INTO Categories(name) VALUES ("Dessert");
