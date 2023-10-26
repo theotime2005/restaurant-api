118ec2412d85358f9c2de8cdbe0fe912827204c9
theotime2005 <theotime.berthod@epitech.digital>
2023-10-26 10:26:39 +0200

    Categories actions are added. Start to writing the documentation and first Formulas opéerations

--- data/category.sql
+++
@@ -1,5 +0,0 @@
--- Ajouter les catégories principales à la table Categories
-
-INSERT INTO Categories(name) VALUES ("Entrée");
-INSERT INTO Categories(name) VALUES ("plat");
-INSERT INTO Categories(name) VALUES ("Dessert");
